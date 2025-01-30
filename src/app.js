80628d10ae71b5c685d433109be45710f73b0a3b
Christian J. Marrero Diaz <christian.marrero3@gmail.com>
2025-01-30 16:18:02 +0000

    commented app.js and interated js into the html with the use o a script instead

--- src/app.js
+++ src/app.js
@@ -2,29 +2,29 @@
 import "bootstrap";
 import "./styles.css";
 
-window.onload = function() {
-  document.querySelectorAll(".color").forEach(item => {
-    item.addEventListener("mousedown", event => {
-      event.currentTarget.classList.add("pressed");
-    });
-    item.addEventListener("mouseup", event => {
-      event.currentTarget.classList.remove("pressed");
-    });
-    item.addEventListener("mouseleave", event => {
-      event.currentTarget.classList.remove("pressed");
-    });
+// window.onload = function() {
+//   document.querySelectorAll(".color").forEach(item => {
+//     item.addEventListener("mousedown", event => {
+//       event.currentTarget.classList.add("pressed");
+//     });
+//     item.addEventListener("mouseup", event => {
+//       event.currentTarget.classList.remove("pressed");
+//     });
+//     item.addEventListener("mouseleave", event => {
+//       event.currentTarget.classList.remove("pressed");
+//     });
 
-    item.addEventListener("click", event => {
-      const colorCode = event.currentTarget.getAttribute("data-color");
-      navigator.clipboard.writeText(colorCode).then(
-        function() {
-          console.log("Color code copied to clipboard:", colorCode);
-          // You can add a visual indication here if needed
-        },
-        function(err) {
-          console.error("Unable to copy color code to clipboard", err);
-        }
-      );
-    });
-  });
-};
+//     item.addEventListener("click", event => {
+//       const colorCode = event.currentTarget.getAttribute("data-color");
+//       navigator.clipboard.writeText(colorCode).then(
+//         function() {
+//           console.log("Color code copied to clipboard:", colorCode);
+//           // You can add a visual indication here if needed
+//         },
+//         function(err) {
+//           console.error("Unable to copy color code to clipboard", err);
+//         }
+//       );
+//     });
+//   });
+// };
